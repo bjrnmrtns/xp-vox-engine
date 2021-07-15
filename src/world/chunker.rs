efcaf6c840556825c60c934306482984b5dae9e6
Bjorn Martens <bjorn@expeditious.nl>
2021-07-15 13:45:31 +0200

    remove model addition to chunker

--- src/world/chunker.rs
+++ src/world/chunker.rs
@@ -36,54 +36,6 @@ impl Chunker {
         }
     }
 
-    pub fn add(&mut self, handle: Handle<Vox>, position: [i32; 3], registry: &Registry<Vox>) {
-        let vox = registry.get(&handle).unwrap();
-        self.entities
-            .push((handle, [vox.x_size, vox.y_size, vox.z_size], position));
-        let x_min = position[0];
-        let y_min = position[1];
-        let z_min = position[2];
-        let mut z_size = vox.z_size;
-        let (mut z_number, mut target_z_offset) = chunk_number_and_offset(z_min, self.chunk_size);
-        let mut source_z_offset = 0;
-        while z_size != 0 {
-            let z_current_size = std::cmp::min(z_size, self.chunk_size - target_z_offset);
-            let mut y_size = vox.y_size;
-            let (mut y_number, mut target_y_offset) = chunk_number_and_offset(y_min, self.chunk_size);
-            let mut source_y_offset = 0;
-            while y_size != 0 {
-                let y_current_size = std::cmp::min(y_size, self.chunk_size - target_y_offset);
-                let mut x_size = vox.x_size;
-                let (mut x_number, mut target_x_offset) = chunk_number_and_offset(x_min, self.chunk_size);
-                let mut source_x_offset = 0;
-                while x_size != 0 {
-                    let x_current_size = std::cmp::min(x_size, self.chunk_size - target_x_offset);
-                    self.chunk_entity_map.insert(
-                        (x_number, y_number, z_number),
-                        (
-                            self.entities.len() - 1,
-                            [source_x_offset, source_y_offset, source_z_offset],
-                            [target_x_offset, target_y_offset, target_z_offset],
-                            [x_current_size, y_current_size, z_current_size],
-                        ),
-                    );
-                    x_number += 1;
-                    source_x_offset += x_current_size;
-                    target_x_offset = 0;
-                    x_size -= x_current_size;
-                }
-                y_number += 1;
-                source_y_offset += y_current_size;
-                target_y_offset = 0;
-                y_size -= y_current_size;
-            }
-            z_number += 1;
-            source_z_offset += z_current_size;
-            target_z_offset = 0;
-            z_size -= z_current_size;
-        }
-    }
-
     pub fn generate_chunk(&mut self, chunk: [i32; 3]) -> (Option<MeshData>, Transform) {
         let mut vox_to_gen = Vox::new(self.chunk_size, self.chunk_size, self.chunk_size);
         for z in 0..self.chunk_size {
